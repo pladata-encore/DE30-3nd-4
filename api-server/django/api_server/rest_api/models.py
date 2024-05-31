a7f0da346841d9d7c65b7f35cb355da12a129a7d
kminssk <kminssk@gmail.com>
2024-05-31 15:43:11 +0900

    MODIFY: models.py, serializers.py of Django api server
    Modified models, serializers based on the database schema defined on the DB server.

--- api-server/django/api_server/rest_api/models.py
+++ api-server/django/api_server/rest_api/models.py
@@ -2,28 +2,34 @@ from django.db import models
 
 
 # Create your models here.
+# User model
 class User(models.Model):
-    _id = models.AutoField(primary_key=True)
-    user_id = models.CharField(max_length=20, unique=True)
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=20)
-    best_score = models.IntegerField()
-    average_score = models.FloatField()
-    ranking = models.IntegerField()
-    play_count = models.IntegerField()
+    best_score = models.IntegerField(default=0)
+    average_score = models.FloatField(default=0)
+    ranking = models.IntegerField(null=True, blank=True)  # Allowing null and blank values
+    play_count = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.user_id
+        return self.name
+
+    class Meta:
+        db_table = 'users'
 
 
+# Game model
 class Game(models.Model):
-    _id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='_id')
+    game_id = models.AutoField(primary_key=True, db_column='game_id')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
     when_played = models.DateTimeField()
     kill_count = models.IntegerField()
     elapsed_time = models.FloatField()
     score = models.IntegerField()
 
-    class Meta:
-        unique_together = ('_id', 'when_played')
-
     def __str__(self):
-        return f'Game played by {self._id.user_id} on {self.when_played}'
+        return f'Game played by {self.user_id.name if self.user_id else "Unknown User"} on {self.when_played}'
+
+    class Meta:
+        db_table = 'games'
--- api-server/django/api_server/rest_api/serializers.py
+++ api-server/django/api_server/rest_api/serializers.py
@@ -5,10 +5,10 @@ from .models import User, Game
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['_id', 'user_id', 'password', 'best_score', 'average_score', 'ranking', 'play_count']
+        fields = ['user_id', 'name', 'password', 'best_score', 'average_score', 'ranking', 'play_count']
 
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = ['_id', 'when_played', 'kill_count', 'elapsed_time', 'score']
+        fields = ['game_id', 'user_id', 'when_played', 'kill_count', 'elapsed_time', 'score']
