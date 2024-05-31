1e4b778a8e1a7677f415e5a9f3af92a74e3c2001
kminssk <kminssk@gmail.com>
2024-05-31 09:24:51 +0900

    CREATE: models.py of django api server
    The models were created based on the database schema defined on the DB server.

--- api-server/django/api_server/rest_api/models.py
+++ api-server/django/api_server/rest_api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    _id = models.AutoField(primary_key=True)
+    user_id = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    best_score = models.IntegerField()
+    average_score = models.FloatField()
+    ranking = models.IntegerField()
+    play_count = models.IntegerField()
+
+    def __str__(self):
+        return self.user_id
+
+
+class Game(models.Model):
+    _id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='_id')
+    when_played = models.DateTimeField()
+    kill_count = models.IntegerField()
+    elapsed_time = models.FloatField()
+    score = models.IntegerField()
+
+    class Meta:
+        unique_together = ('_id', 'when_played')
+
+    def __str__(self):
+        return f'Game played by {self._id.user_id} on {self.when_played}'
