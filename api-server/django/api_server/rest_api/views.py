0744ac507cb037fbaa7d504968a816cc81ae4e14
kminssk <kminssk@gmail.com>
2024-05-31 15:45:36 +0900

    CREATE: views.py of Django api server
    Created skeleton views for testing server.

--- api-server/django/api_server/rest_api/views.py
+++ api-server/django/api_server/rest_api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User, Game
+from .serializers import UserSerializer, GameSerializer
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def user_list(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        print(users)
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+def game_list(request):
+    if request.method == 'GET':
+        games = Game.objects.all()
+        print(games)
+        serializer = GameSerializer(games, many=True)
+        return Response(serializer.data)
