092577bbe935ae7717f235c50f02ac20f848cac8
kminssk <kminssk@gmail.com>
2024-05-31 15:39:28 +0900

    MODIFY: settings.py of django api server
    Integrated an external MySQL DB server with the Django api server.

--- api-server/django/api_server/api_server/settings.py
+++ api-server/django/api_server/api_server/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'api_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hehebalssa',
+        'USER': 'root',
+        'PASSWORD': '7276',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
