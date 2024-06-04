9fd4bb395c329eea565ad1f1e4c4d12bd0ebb2d2
kminssk <kminssk@gmail.com>
2024-06-04 18:46:31 +0900

    FIX: game_urls.py
    Corrected some wrong coding.

--- api-server/django/api_server/rest_api/urls/game_urls.py
+++ api-server/django/api_server/rest_api/urls/game_urls.py
@@ -5,6 +5,6 @@ from rest_api.views.game_views import savegame, leaderboard
 
 
 urlpatterns = [
-    path('register/', savegame, name='savegame'),
+    path('savegame/', savegame, name='savegame'),
     path('leaderboard/', leaderboard, name='leaderboard'),
 ]
